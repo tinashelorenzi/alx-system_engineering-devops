726d11d05219a1a91b6a00f7d131a406f74f9246
Tinashe <tinashelorenzi@protonmail.com>
2023-08-10 01:10:06 +0200

    puppet automated fix

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# automated puppet fix
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,5 @@
+.
+├── 0-strace_is_your_friend.pp
+└── README.md
+
+0 directories, 2 files
